66a821f9a7b73aa2107cae8ed83d338afc81a829
Tim Gold <timgold@gmail.com>
2017-12-20 08:17:50 +0000

    Added down counter - counts X pings and then declares when host is down

--- pinger.py
+++ pinger.py
@@ -4,8 +4,10 @@ import os
 import time
 import datetime
 
-up_interval=3
+up_interval=1
 down_interval=1
+#how many ping need to fail before the host declared as down
+down_counter=3
 if len(sys.argv)==1:
     ip_addr="8.8.8.8"
 else:
@@ -13,6 +15,8 @@ else:
 status="unknown"
 prev_status="unknown"
 
+
+down_cnt=down_counter
 while 1:
     # print ("DEBUG$$$: status: "+status+" prev_status:"+prev_status)
     res=os.system("ping -c 1 -w 1 " + ip_addr + " >/dev/null")
@@ -27,12 +31,16 @@ while 1:
         time.sleep(up_interval)
     else:
         status="down"
-        if prev_status=="unknown":
-            prev_status="down"
-            print ("system " + ip_addr + " down at " + str(datetime.datetime.now()))
-        elif prev_status=="up":
-            prev_status="down"
-            print ("system " + ip_addr + " down at " + str(datetime.datetime.now()))
+	if down_cnt>0:
+            down_cnt=down_cnt-1
+        else:
+            down_cnt=down_counter
+            if prev_status=="unknown":
+                prev_status="down"
+                print ("system " + ip_addr + " down at " + str(datetime.datetime.now()))
+            elif prev_status=="up":
+                prev_status="down"
+                print ("system " + ip_addr + " down at " + str(datetime.datetime.now()))
         time.sleep(down_interval)
 
 
