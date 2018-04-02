9f9864044098e545b1ea58572bbc0de04414ac59
timgold81 <timgold@gmail.com>
2018-04-02 10:34:34 +0300

    added argument parsing

--- pinger.py
+++ pinger.py
@@ -3,23 +3,64 @@ import sys
 import os
 import time
 import datetime
+import argparse
+import signal
+import platform
 
-up_interval=1
-down_interval=1
-#how many ping need to fail before the host declared as down
-down_counter=3
-if len(sys.argv)==1:
-    ip_addr="8.8.8.8"
+def signal_handler(signal,frame):
+    global loop
+    loop=0
+    print ("\nStopping pinger. Thanks for using.")
+    print ("Please visit https://github.com/timgold81/")
+    print ("contact timgold@gmail.com\n")
+
+
+signal.signal(signal.SIGINT,signal_handler)
+
+parser=argparse.ArgumentParser(description="Ping tracker of a host. Shows the time when the host went UP or DOWN")
+parser.add_argument("-s","--host",help="Host name or IP address. Default = 8.8.8.8")
+parser.add_argument("-d","--down",help="How many ping get lost, until the host declared as DOWN. Default=3")
+parser.add_argument("-o","--down_interval",help="Interval between pings when host is down. Default=1")
+parser.add_argument("-u","--up_interval",help="Interval between pings when host is up. Default = 1")
+args=parser.parse_args()
+
+if args.host:
+    ip_addr=args.host
 else:
-    ip_addr=sys.argv[1]
+    ip_addr="8.8.8.8"
+
+if args.down:
+    down_counter=int(args.down)
+else:
+    down_counter=3
+
+if args.down_interval:
+    down_interval=float(args.down_interval)
+else:
+    down_interval=1
+
+if args.up_interval:
+    up_interval=float(args.up_interval)
+else:
+    up_interval=1
+
 status="unknown"
 prev_status="unknown"
-
-
 down_cnt=down_counter
-while 1:
+loop=1
+
+if  platform.system()=="Windows":
+    print ("Pingtracker down no operate well under Windows")
+    print ("Try linux or ubuntu under Windows 10")
+
+print ("Tracking "+ip_addr +":")
+
+while loop:
     # print ("DEBUG$$$: status: "+status+" prev_status:"+prev_status)
-    res=os.system("ping -c 1 -w 1 " + ip_addr + " >/dev/null")
+    if platform.system()=="Linux":
+        res=os.system("ping -c 1 -w 1 " + ip_addr + " >/dev/null")
+    elif platform.system()=="Windows":
+        res=os.system("ping -n 1 -w 1000 " + ip_addr + "> nul")
     if res==0:
         status="up"
         if prev_status=="unknown":
@@ -31,7 +72,7 @@ while 1:
         time.sleep(up_interval)
     else:
         status="down"
-	if down_cnt>0:
+        if down_cnt>0:
             down_cnt=down_cnt-1
         else:
             down_cnt=down_counter
@@ -42,6 +83,3 @@ while 1:
                 prev_status="down"
                 print ("system " + ip_addr + " down at " + str(datetime.datetime.now()))
         time.sleep(down_interval)
-
-
-
