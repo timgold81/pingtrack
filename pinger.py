92bde87ab77c86e8ee29bd2d5482ed5e5d0a8b08
Tim Goldshmit <tim.goldshmit@audiocodes.com>
2018-10-15 08:56:41 +0300

    Created as exportable class

--- pinger.py
+++ pinger.py
@@ -27,75 +27,75 @@ class configuration:
     ip_addr=""
 
 
-def handle_args():
-    parser=argparse.ArgumentParser(description="Ping tracker of a host. Shows the time when the host went UP or DOWN")
-    parser.add_argument("-s","--host",help="Host name or IP address. Default = 8.8.8.8")
-    parser.add_argument("-d","--down",help="How many pings get lost, until the host declared as DOWN. Default=3")
-    parser.add_argument("-o","--down_interval",help="Interval between pings when host is down. Default=1")
-    parser.add_argument("-u","--up_interval",help="Interval between pings when host is up. Default=1")
-    args=parser.parse_args()
+    def handle_args(self):
+        parser=argparse.ArgumentParser(description="Ping tracker of a host. Shows the time when the host went UP or DOWN")
+        parser.add_argument("-s","--host",help="Host name or IP address. Default = 8.8.8.8")
+        parser.add_argument("-d","--down",help="How many pings get lost, until the host declared as DOWN. Default=3")
+        parser.add_argument("-o","--down_interval",help="Interval between pings when host is down. Default=1")
+        parser.add_argument("-u","--up_interval",help="Interval between pings when host is up. Default=1")
+        args=parser.parse_args()
 
-    if args.host:
-        conf.ip_addr=args.host
-    else:
-        conf.ip_addr="8.8.8.8"
-
-    if args.down:
-        
-        conf.down_counter=int(args.down)
-    else:
-        conf.down_counter=3
-
-    if args.down_interval:
-        conf.down_interval=float(args.down_interval)
-    else:
-        conf.down_interval=1
-
-    if args.up_interval:
-        conf.up_interval=float(args.up_interval)
-    else:
-        conf.up_interval=1
-
-    conf.status="unknown"
-    conf.prev_status="unknown"
-    conf.down_cnt=conf.down_counter
-    conf.loop=1
-
-def main():
-    print ("Tracking "+conf.ip_addr +":")
-    while conf.loop:
-        # print ("DEBUG$$$: status: "+status+" prev_status:"+prev_status)
-        if platform.system()=="Linux":
-            res=os.system("ping -c 1 -w 1 " + conf.ip_addr + " >/dev/null")
-        elif platform.system()=="Windows":
-            res=os.system("ping -n 1 -w 1000 " + conf.ip_addr + "> nul")
-        if res==0:
-            conf.status="up"
-            if conf.prev_status=="unknown":
-                conf.prev_status="up"
-                print ("system " + conf.ip_addr + " up at " + str(datetime.datetime.now()))
-            elif conf.prev_status=="down":
-                conf.prev_status = "up"
-                print ("system " + conf.ip_addr + " up at " + str(datetime.datetime.now()))
-            time.sleep(conf.up_interval)
+        if args.host:
+            self.ip_addr=args.host
         else:
-            conf.status="down"
-            if conf.down_cnt>0:
-                conf.down_cnt=conf.down_cnt-1
+            self.ip_addr="8.8.8.8"
+
+        if args.down:
+            
+            self.down_counter=int(args.down)
+        else:
+            self.down_counter=3
+
+        if args.down_interval:
+            self.down_interval=float(args.down_interval)
+        else:
+            self.down_interval=1
+
+        if args.up_interval:
+            self.up_interval=float(args.up_interval)
+        else:
+            self.up_interval=1
+
+        self.status="unknown"
+        self.prev_status="unknown"
+        self.down_cnt=self.down_counter
+        self.loop=1
+
+    def main(self):
+        print ("Tracking "+self.ip_addr +":")
+        while self.loop:
+            # print ("DEBUG$$$: status: "+status+" prev_status:"+prev_status)
+            if platform.system()=="Linux":
+                res=os.system("ping -c 1 -w 1 " + self.ip_addr + " >/dev/null")
+            elif platform.system()=="Windows":
+                res=os.system("ping -n 1 -w 1000 " + self.ip_addr + "> nul")
+            if res==0:
+                self.status="up"
+                if self.prev_status=="unknown":
+                    self.prev_status="up"
+                    print ("system " + self.ip_addr + " up at " + str(datetime.datetime.now()))
+                elif self.prev_status=="down":
+                    self.prev_status = "up"
+                    print ("system " + self.ip_addr + " up at " + str(datetime.datetime.now()))
+                time.sleep(self.up_interval)
             else:
-                conf.down_cnt=conf.down_counter
-                if conf.prev_status=="unknown":
-                    conf.prev_status="down"
-                    print ("system " + conf.ip_addr + " down at " + str(datetime.datetime.now()))
-                elif conf.prev_status=="up":
-                    conf.prev_status="down"
-                    print ("system " + conf.ip_addr + " down at " + str(datetime.datetime.now()))
-            time.sleep(conf.down_interval)
+                self.status="down"
+                if self.down_cnt>0:
+                    self.down_cnt=self.down_cnt-1
+                else:
+                    self.down_cnt=self.down_counter
+                    if self.prev_status=="unknown":
+                        self.prev_status="down"
+                        print ("system " + self.ip_addr + " down at " + str(datetime.datetime.now()))
+                    elif self.prev_status=="up":
+                        self.prev_status="down"
+                        print ("system " + self.ip_addr + " down at " + str(datetime.datetime.now()))
+                time.sleep(self.down_interval)
 
 
 if __name__=="__main__":
     signal.signal(signal.SIGINT,signal_handler)
     global conf
     conf=configuration()
-    handle_args()
-    main()
+    conf.handle_args()
+    conf.main()
