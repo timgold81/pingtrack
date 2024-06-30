7be8c5d1ebb61bac436ade3a9642b9c0ed5430fd
Tim Gold <timgold@gmail.com>
2024-06-30 08:20:24 +0000

    code impovements

--- pinger.py
+++ pinger.py
@@ -6,95 +6,68 @@ import datetime
 import argparse
 import signal
 import platform
-
-def signal_handler(signal,frame):
-    # global conf
-    pinger.loop=0
-    print ("\nStopping pinger. Thanks for using.")
-    print ("Please visit https://github.com/timgold81/")
-    print ("contact timgold@gmail.com\n")
-
-
+import subprocess
 
 class Pinger:
-    status="unknown"
-    prev_status="unknown"
-    down_counter=3
-    down_cnt=3
-    down_interval=1
-    up_interval=1
-    loop=1
-    ip_addr="8.8.8.8"
+    def __init__(self):
+        self.status = "unknown"
+        self.prev_status = "unknown"
+        self.down_counter = 3
+        self.down_cnt = 3
+        self.down_interval = 1
+        self.up_interval = 1
+        self.loop = 1
+        self.ip_addr = "8.8.8.8"
 
+    def signal_handler(self, signal, frame):
+        self.loop = 0
+        print("\nStopping pinger. Thanks for using.")
+        print("Please visit https://github.com/timgold81/")
+        print("contact timgold@gmail.com\n")
 
     def handle_args(self):
-        parser=argparse.ArgumentParser(description="Ping tracker of a host. Shows the time when the host went UP or DOWN")
-        parser.add_argument("-s","--host",help="Host name or IP address. Default = 8.8.8.8")
-        parser.add_argument("-d","--down",help="How many pings get lost, until the host declared as DOWN. Default=3")
-        parser.add_argument("-o","--down_interval",help="Interval between pings when host is down. Default=1")
-        parser.add_argument("-u","--up_interval",help="Interval between pings when host is up. Default=1")
-        args=parser.parse_args()
+        parser = argparse.ArgumentParser(description="Ping tracker of a host. Shows the time when the host went UP or DOWN")
+        parser.add_argument("-s", "--host", default="8.8.8.8", help="Host name or IP address.")
+        parser.add_argument("-d", "--down", default=3, type=int, help="How many pings get lost, until the host declared as DOWN.")
+        parser.add_argument("-o", "--down_interval", default=1, type=float, help="Interval between pings when host is down.")
+        parser.add_argument("-u", "--up_interval", default=1, type=float, help="Interval between pings when host is up.")
+        args = parser.parse_args()
 
-        if args.host:
-            self.ip_addr=args.host
-        else:
-            self.ip_addr="8.8.8.8"
+        self.ip_addr = args.host
+        self.down_counter = args.down
+        self.down_interval = args.down_interval
+        self.up_interval = args.up_interval
+        self.down_cnt = self.down_counter
 
-        if args.down:
-            self.down_counter=int(args.down)
-        else:
-            self.down_counter=3
-
-        if args.down_interval:
-            self.down_interval=float(args.down_interval)
-        else:
-            self.down_interval=1
-
-        if args.up_interval:
-            self.up_interval=float(args.up_interval)
-        else:
-            self.up_interval=1
-
-        self.status="unknown"
-        self.prev_status="unknown"
-        self.down_cnt=self.down_counter
-        self.loop=1
+    def print_status(self, status):
+        if self.prev_status != status:
+            self.prev_status = status
+            print("system " + self.ip_addr + " " + status + " at " + str(datetime.datetime.now()))
 
     def start(self):
-        print ("Tracking "+self.ip_addr +":")
+        print("Tracking " + self.ip_addr + ":")
         while self.loop:
-            # print ("DEBUG$$$: status: "+status+" prev_status:"+prev_status)
-            if platform.system()=="Linux":
-                res=os.system("ping -c 1 -w 1 " + self.ip_addr + " >/dev/null")
-            elif platform.system()=="Windows":
-                res=os.system("ping -n 1 -w 1000 " + self.ip_addr + "> nul")
-            if res==0:
-                self.status="up"
-                if self.prev_status=="unknown":
-                    self.prev_status="up"
-                    print ("system " + self.ip_addr + " up at " + str(datetime.datetime.now()))
-                elif self.prev_status=="down":
-                    self.prev_status = "up"
-                    print ("system " + self.ip_addr + " up at " + str(datetime.datetime.now()))
+            if platform.system() == "Linux":
+                res = subprocess.call(["ping", "-c", "1", "-w", "1", self.ip_addr], stdout=subprocess.DEVNULL)
+            elif platform.system() == "Windows":
+                res = subprocess.call(["ping", "-n", "1", "-w", "1000", self.ip_addr], stdout=subprocess.DEVNULL, shell=True)
+            if res == 0:
+                self.status = "up"
+                self.print_status(self.status)
                 time.sleep(self.up_interval)
             else:
-                self.status="down"
-                if self.down_cnt>0:
-                    self.down_cnt=self.down_cnt-1
+                self.status = "down"
+                if self.down_cnt > 0:
+                    self.down_cnt -= 1
                 else:
-                    self.down_cnt=self.down_counter
-                    if self.prev_status=="unknown":
-                        self.prev_status="down"
-                        print ("system " + self.ip_addr + " down at " + str(datetime.datetime.now()))
-                    elif self.prev_status=="up":
-                        self.prev_status="down"
-                        print ("system " + self.ip_addr + " down at " + str(datetime.datetime.now()))
+                    self.down_cnt = self.down_counter
+                    self.print_status(self.status)
                 time.sleep(self.down_interval)
 
 
-if __name__=="__main__":
-    signal.signal(signal.SIGINT,signal_handler)
-    global pinger
-    pinger=Pinger()
+if __name__ == "__main__":
+    pinger = Pinger()
+    signal.signal(signal.SIGINT, pinger.signal_handler)
     pinger.handle_args()
     pinger.start()
+
